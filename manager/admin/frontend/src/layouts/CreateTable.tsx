dcc67b1bb27e0c09a708a2975adf8e44c2c9d1c2
Fumiaki Nagashima <info@alcogy.com>
2024-11-08 18:05:31 +0900

    Refactoring.

--- manager/admin/frontend/src/layouts/CreateTable.tsx
+++ manager/admin/frontend/src/layouts/CreateTable.tsx
@@ -2,21 +2,12 @@ import Typography from "@mui/material/Typography";
 import ModuleTitle from "../components/ModuleTitle";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import {
-  Checkbox,
-  FormControl,
-  FormControlLabel,
-  IconButton,
-  InputLabel,
-  MenuItem,
-  Select,
-  TextField,
-} from "@mui/material";
-import DeleteIcon from "@mui/icons-material/Delete";
-import { useEffect, useState } from "react";
+import { IconButton, TextField } from "@mui/material";
+import { useCallback, useEffect, useState } from "react";
 import AddBoxIcon from "@mui/icons-material/AddBox";
-import { API_TABLE_CREATE, segmentList, typeList } from "../common/constants";
+import { API_TABLE_CREATE, typeList } from "../common/constants";
 import { Column, ColumnFormParams } from "../state/models";
+import TableForm from "../components/TableForm";
 
 export const initColunInfo: Column = {
   name: "",
@@ -147,144 +138,32 @@ export default function CreateTable() {
         />
 
         <Box>
-          <Box
-            sx={{
-              display: "flex",
-              gap: 8,
-              alignItems: "center",
-              marginBottom: 3,
-            }}
+          <Typography
+            variant="subtitle1"
+            sx={{ fontWeight: "bold", marginBottom: 2 }}
           >
-            <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
-              Setting Columns
-            </Typography>
-            <IconButton
-              onClick={() => setColumns([...columns, { ...initColunInfo }])}
-            >
-              <AddBoxIcon />
-            </IconButton>
-          </Box>
+            Setting Columns
+          </Typography>
           <Box sx={{ display: "flex", flexDirection: "column", gap: 3 }}>
             {columns.length === 0 && (
               <Typography>Please add column...</Typography>
             )}
             {columns.map((value, index) => (
-              <Box sx={{ display: "flex", gap: 1 }} key={index}>
-                <TextField
-                  label="Column name"
-                  variant="outlined"
-                  value={value.name}
-                  onChange={(e) => updateForm(index, "name", e.target.value)}
-                />
-                <FormControl sx={{ minWidth: "128px" }}>
-                  <InputLabel id="select-label">Column Type</InputLabel>
-                  <Select
-                    label="Column Type"
-                    labelId="select-label"
-                    defaultValue={10}
-                    value={value.type}
-                    onChange={(e) => updateForm(index, "type", e.target.value)}
-                  >
-                    {typeList.map((v) => (
-                      <MenuItem value={v.value} key={v.value}>
-                        {v.label}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-
-                <TextField
-                  label="Size"
-                  variant="outlined"
-                  value={value.size}
-                  sx={{ width: "64px" }}
-                  onChange={(e) => updateForm(index, "size", e.target.value)}
-                  disabled={![20].includes(value.type)}
-                />
-
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={value.pk}
-                      onChange={(e) =>
-                        updateForm(index, "pk", e.target.checked)
-                      }
-                    />
-                  }
-                  label="Primary key"
-                />
-
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={value.notNull}
-                      disabled={value.pk}
-                      onChange={(e) =>
-                        updateForm(index, "notNull", e.target.checked)
-                      }
-                    />
-                  }
-                  label="Not Null"
-                />
-
-                <FormControl sx={{ minWidth: "88px" }}>
-                  <InputLabel id="unique-label">Unique</InputLabel>
-                  <Select
-                    label="Unique"
-                    labelId="unique-label"
-                    defaultValue={0}
-                    value={value.unique}
-                    onChange={(e) =>
-                      updateForm(index, "unique", e.target.value)
-                    }
-                  >
-                    {segmentList.map((v) => (
-                      <MenuItem value={v.value} key={v.value}>
-                        {v.label}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-
-                <FormControl sx={{ minWidth: "88px" }}>
-                  <InputLabel id="index-label">Index</InputLabel>
-                  <Select
-                    label="Index"
-                    labelId="index-label"
-                    defaultValue={0}
-                    value={value.index}
-                    onChange={(e) => updateForm(index, "index", e.target.value)}
-                  >
-                    {segmentList.map((v) => (
-                      <MenuItem value={v.value} key={v.value}>
-                        {v.label}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-
-                <TextField
-                  label="Remarks"
-                  variant="outlined"
-                  value={value.comment}
-                  sx={{ width: "256px" }}
-                  onChange={(e) => updateForm(index, "comment", e.target.value)}
-                />
-
-                <Box
-                  sx={{
-                    flex: 1,
-                    display: "flex",
-                    alignItems: "center",
-                    marginLeft: 3,
-                  }}
-                >
-                  <IconButton onClick={() => onClickDeleteRow(index)}>
-                    <DeleteIcon fontSize="small" />
-                  </IconButton>
-                </Box>
-              </Box>
+              <TableForm
+                key={index}
+                value={value}
+                index={index}
+                updateForm={updateForm}
+                onClickDeleteRow={onClickDeleteRow}
+              />
             ))}
+          </Box>
+          <Box sx={{ marginTop: 1 }}>
+            <IconButton
+              onClick={() => setColumns([...columns, { ...initColunInfo }])}
+            >
+              <AddBoxIcon />
+            </IconButton>
           </Box>
         </Box>
 
